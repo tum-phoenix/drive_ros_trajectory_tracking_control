4a22696755292585131a166dac5ceab1c9f7c5e5
xXBasti <basti.lay@gmx.de>
2019-07-13 16:52:47 +0200

    WIP flatness controller

---
+++ include/drive_ros_trajectory_tracking_control/flatness_controller.h
@@ -0,0 +1,14 @@
+//
+// Created by sebastian on 11.07.19.
+//
+
+#ifndef SRC_FLATNESS_CONTROLLER_H
+#define SRC_FLATNESS_CONTROLLER_H
+
+
+class flatness_controller {
+
+};
+
+
+#endif //SRC_FLATNESS_CONTROLLER_H
---
+++ src/flatness_controller.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sebastian on 11.07.19.
+//
+
+#include "flatness_controller.h"
