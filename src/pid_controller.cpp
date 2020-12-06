36ea402c392d8dab4515ef514bf8a602c722eac6
Phillip Pitschi <phillip.pitschi@tum.de>
2020-12-06 15:25:26 +0100

    new pid controller

--- src/pid_controller.cpp
+++ src/pid_controller.cpp
@@ -1,6 +1,8 @@
-#include "../include/drive_ros_trajectory_tracking_control/carrot_controller.h"
 
-CarrotController::CarrotController(ros::NodeHandle nh, ros::NodeHandle pnh):
+
+#include "../include/drive_ros_trajectory_tracking_control/pid_controller.h"
+
+PIDController::PIDController(ros::NodeHandle nh, ros::NodeHandle pnh):
         TrajectoryTrackingController(nh, pnh) {
 
 //    pnh_.getParam("penalty_y", weight_y_);
@@ -10,48 +12,62 @@ CarrotController::CarrotController(ros::NodeHandle nh, ros::NodeHandle pnh):
     //von config einlesen, um live einzustellen
 
 //    mpcParameters.stepSize = 0.1; //Zeitschrittgroesse fuer MPC
-    trajectory_sub_ = nh.subscribe("local_trajectory", 1, &CarrotController::trajectoryCB, this);
+    trajectory_sub_ = nh.subscribe("local_trajectory", 1, &PIDController::trajectoryCB, this);
+    esum=0;
+    eold=0;
 }
 
 
-CarrotController::~CarrotController() {}
+PIDController::~PIDController() {}
 
-void CarrotController::trajectoryCB(const drive_ros_msgs::TrajectoryConstPtr &msg) {
+void PIDController::trajectoryCB(const drive_ros_msgs::TrajectoryConstPtr &msg) {
 
-    auto carrot_x = msg->points[4].pose.x;
-    auto carrot_y = msg->points[4].pose.y;
-    auto carrot_vx = msg->points[4].twist.x;
-    auto carrot_vy = msg->points[4].twist.y;
+    auto pid_x = msg->points[0].pose.x;
+    auto pid_y = msg->points[0].pose.y;
+    auto pid_vx = msg->points[0].twist.x;
+    auto pid_vy = msg->points[0].twist.y;
 
-    float forwardDistanceX = carrot_x;
-    // get y from polynom
-    float forwardDistanceY = 0.f;
+
+    // get distance x
+    float forwardDistanceX = pid_x;
+    // get distance y
+    float forwardDistanceY = pid_y;
 
     // handle lane changes and hard-coded turn/drive commands
     float laneChangeDistance = 0.f;
-    float presetSteeringAngle;
-    bool steeringAngleFixed = false;
     bool steerFrontAndRear = false;
 
     drive_ros_uavcan::phoenix_msgs__NucDriveCommand::_blink_com_type blink_com =
             drive_ros_uavcan::phoenix_msgs__NucDriveCommand::NO_BLINK;
 
-    forwardDistanceY = carrot_y;
-
+    //calculate steering angle
+        //control error
+        float e=pid_y;
+        esum+=e;
     float kappa;
-    if (!steeringAngleFixed)
-        kappa = (std::atan2(forwardDistanceY, forwardDistanceX));
-    else
-        kappa = presetSteeringAngle;
+        kappa = k_p*e+k_d*(e-eold)+k_i*esum;
 
+    //update control errors
+    eold=e;
+
+    //saturation
+    if (kappa>(22.f*M_PI/180.f)){
+        kappa=22.f*M_PI/180.f;
+    }
+    else if(kappa<(-22.f*M_PI/180.f)){
+        kappa=-22.f*M_PI/180.f;
+    }
+
+    //print goal point and steering angle
     ROS_INFO_NAMED(stream_name_, "Goal point (%.2f, %.2f)", forwardDistanceX, forwardDistanceY);
-    ROS_INFO_NAMED(stream_name_, "Kappa: = %.5f", kappa);
+    ROS_INFO_NAMED(stream_name_, "Kappa = %.5f", kappa);
 
-    // TODO: querbeschleunigung
+    // TODO: laengsbeschleunigung
 
-    float vGoal = carrot_vx;
+    float vGoal = pid_vx;
 
     ROS_INFO_NAMED(stream_name_, "vGoal = %f", vGoal);
+
 
     drive_ros_uavcan::phoenix_msgs__NucDriveCommand driveCmdMsg;
     driveCmdMsg.phi_f = kappa;
@@ -59,7 +75,7 @@ void CarrotController::trajectoryCB(const drive_ros_msgs::TrajectoryConstPtr &ms
         driveCmdMsg.phi_r = 0.0f;
     else
         driveCmdMsg.phi_r = kappa;
-    driveCmdMsg.lin_vel = carrot_vx;
+    driveCmdMsg.lin_vel = vGoal;
     driveCmdMsg.blink_com = blink_com;
 
     //if(!isnanf(steeringAngleFront) && !isnanf(steeringAngleRear)) {
--- src/trajectory_tracking_control_node.cpp
+++ src/trajectory_tracking_control_node.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <drive_ros_trajectory_tracking_control/model_predictive_controller.h>
 #include <drive_ros_trajectory_tracking_control/carrot_controller.h>
+#include <drive_ros_trajectory_tracking_control/pid_controller.h>
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "trajectory_tracking_control_node");
@@ -8,7 +9,8 @@ int main(int argc, char **argv) {
     ros::NodeHandle pnh("~");
 
 //    ModelPredictiveController controller(nh, pnh);
-    CarrotController controller(nh, pnh);
+//    CarrotController controller(nh, pnh);
+	PIDController controller(nh, pnh);
     ros::Rate loop_rate(20);
     while (ros::ok()) {
         ros::spin();
